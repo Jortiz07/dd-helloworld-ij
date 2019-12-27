862c4a8e63bd5b6c92473dc386ebb85a919cf704
jortiz07 <jortiz192@cnm.edu>
2019-12-27 15:46:32 -0700

    Add salutation method to construct and return a salutation string.

--- src/jortiz07/helloworld/prework/HelloWorld.java
+++ src/jortiz07/helloworld/prework/HelloWorld.java
@@ -4,10 +4,12 @@ import javax.swing.DefaultListSelectionModel;
 
 public class HelloWorld {
   public static void main(String[] args) {
-    salute((args.length > 0) ? args[0] :"World");
+    salute(salutation(args));
 
     }
-
+    private static String salutation(String[] args) {
+    return "Hello, " + ((args.length > 0) ? args[0] : "World");
+    }
   private static void salute(String target) {
     System.out.println("Hello, " + target + "!");
   }
