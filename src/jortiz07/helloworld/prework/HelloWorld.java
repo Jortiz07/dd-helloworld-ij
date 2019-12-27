4f2c40e8c65d5a9183dabcea43b6be8c913e3902
jortiz07 <jortiz192@cnm.edu>
2019-12-27 12:11:06 -0700

    Initial Commit Message for Hello World Project

--- src/jortiz07/helloworld/prework/HelloWorld.java
+++ src/jortiz07/helloworld/prework/HelloWorld.java
@@ -6,6 +6,8 @@ public class HelloWorld {
   public static void main(String[] args) {
     if (args.length > 0) {
       salute(args[0]);
+    } else {
+      salute("World");
     }
   }
   private static void salute(String target) {
