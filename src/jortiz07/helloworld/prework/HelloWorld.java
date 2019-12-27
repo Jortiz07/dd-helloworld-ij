e49ce4cd8ec33a066736f6f7b2b24e0ec23cb282
jortiz07 <jortiz192@cnm.edu>
2019-12-27 12:17:16 -0700

    Initial Commit Message for Hello World Project

--- src/jortiz07/helloworld/prework/HelloWorld.java
+++ src/jortiz07/helloworld/prework/HelloWorld.java
@@ -4,12 +4,10 @@ import javax.swing.DefaultListSelectionModel;
 
 public class HelloWorld {
   public static void main(String[] args) {
-    if (args.length > 0) {
-      salute(args[0]);
-    } else {
-      salute("World");
+    salute((args.length > 0) ? args[0] :"World");
+
     }
-  }
+
   private static void salute(String target) {
     System.out.println("Hello, " + target + "!");
   }
